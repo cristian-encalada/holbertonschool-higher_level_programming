6dfb42c82f1d9dc93bfa8fd0347b64414541a0b6
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-05-29 11:40:40 -0300

    [5] Print first, middle and last letters

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]  # first 3 characters
+word_last_2 = word[-2:]   # last 2 characters
+middle_word = word[1:7]   # middle characters
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
