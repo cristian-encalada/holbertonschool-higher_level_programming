17b67ebac3092362527cc66904dc5fc38cb45a15
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-05-30 16:50:04 -0300

    [6] Fix: Remove last comma at the end

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
     for units_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, units_digit), end=', ')
+        print("{:d}{:d}".format(tens_digit, units_digit), end='')
+        if tens_digit != 8 or units_digit != 9:
+            print(', ', end='')
 print()
