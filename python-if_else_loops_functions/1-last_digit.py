49e49ca7db6e542a45decad8833888348bb28d88
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-05-30 11:35:50 -0300

    [1] Calculate and print the last digit of a random signed number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# Calculate the last digit
+last = abs(number) % 10
+# Print the result
+if last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+elif last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+else:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
