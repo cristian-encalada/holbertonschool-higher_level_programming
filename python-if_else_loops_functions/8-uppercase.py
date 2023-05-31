a7ca617f49cd29635f1ed488441de2865f7bdf38
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-05-30 23:07:42 -0300

    [8] Use islower auxiliar function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+def islower(c):
+    return ord(c) in range(ord('a'), ord('z') + 1)
+
+
 def uppercase(str):
     converted = ""
     for c in str:
-        if 'a' <= c <= 'z':
+        if islower(c):
             c = chr(ord(c) + ord('A') - ord('a'))
         converted += c
     print(converted)
