17244bec7c225f8a29aeeb6ab1258f2bff7de2ca
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-05-31 09:52:56 -0300

    [8] Fix removing extra function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    return ord(c) in range(ord('a'), ord('z') + 1)
-
-
 def uppercase(str):
     for c in str:
-        if islower(c):
+        if ord(c) in range(ord('a'), ord('z') + 1):
             c = chr(ord(c) - 32)
-        print(c, end='')
+        print("{}, end=''".format(c))
     print()
