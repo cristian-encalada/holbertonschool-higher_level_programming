668ddfbed7b7884abf19910df472ac6c904b6d94
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-01 17:47:07 -0300

    [4] Try to Use dir() instead of dis()

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    import dis
-    list = dis.dis(hidden_4)
-    for str in list:
-        if str[:2] != "__":
-            print("{}".format(str))
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
