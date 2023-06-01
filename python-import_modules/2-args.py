cafa4aa0c476ace0c97e12bfc88904164a59bb9b
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-01 15:36:30 -0300

    [2] Handle case for a single argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
-import sys
-
 if __name__ == "__main__":
+    import sys
     argc = len(sys.argv) - 1
     if argc == 0:
         print(f"{argc} arguments.")
+    elif argc == 1:
+        print(f"{argc} argument:")
+        print(f"{argc}: {sys.argv[argc]}")
     else:
         print(f"{argc} arguments:")
         for i in range(1, argc + 1):
