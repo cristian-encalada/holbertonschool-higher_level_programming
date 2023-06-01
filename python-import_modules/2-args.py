2c59d4857c70c976e8b8c81a435939258c93586e
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-01 15:28:49 -0300

    [2] Print the number and the list of argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print(f"{argc} arguments.")
+    else:
+        print(f"{argc} arguments:")
+        for i in range(1, argc + 1):
+            print(f"{i}: {sys.argv[i]}")
