e10c6e5bad70b7795caf7598ca76c2980f824db4
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-01 17:38:45 -0300

    [4] Add if condition to filter __ strings

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     import dis
-    print(dis.dis(hidden_4))
+    list = dis.dis(hidden_4)
+    for str in list:
+        if str[:2] != "__":
+            print("{}".format(str))
