03d7a71cd9e1b28f30d6e2e79db7f9ec3ab292f2
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-02 19:34:04 -0300

    [6] Fix case when argv != 3

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+    if len(sys.argv) - 1 != 3:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        exit(1)
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-    argc = len(sys.argv) - 1
-    if argc != 3:
-        print(f"Usage: {arg[0]} {a} {operator} {b}")
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == '-':
