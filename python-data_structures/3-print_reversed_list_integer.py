ca8a987e6eedbf37eb8bd8f6bb3b544c681abed6
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-05 18:29:55 -0300

    [3] Fix: Add condition to check if the list is empty

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for n in reversed(my_list):
-        print("{:d}".format(n))
+    if my_list:  # check if the list is not empty
+        for n in reversed(my_list):
+            print("{:d}".format(n))
