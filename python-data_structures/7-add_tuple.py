3bd92f78b4839f1827601ad1608857b2a67e582f
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-06 10:05:33 -0300

    [7] Fix: typo about a0 and b0

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     # First elements of each tuple
     a0 = tuple_a[0] if len(tuple_a) >= 1 else 0
-    b0 = tuple_a[0] if len(tuple_b) >= 1 else 0
+    b0 = tuple_b[0] if len(tuple_b) >= 1 else 0
     # Second elements for each tuple
     a1 = tuple_a[1] if len(tuple_a) >= 2 else 0
     b1 = tuple_b[1] if len(tuple_b) >= 2 else 0
