9d5b52cbf6f78d665a2f0cff0d9898d0a9fcee7a
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-06 19:28:13 -0300

    [1] Fix: Use for/if instead of replace()

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = [str(n).replace(str(search), str(replace)) for n in my_list]
-    return my_list
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list[i] = replace
+    return new_list
