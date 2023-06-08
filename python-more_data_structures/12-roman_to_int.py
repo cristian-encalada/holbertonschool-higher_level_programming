15032dfd99afc23635011041e2110603a53f4f9d
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-08 16:47:25 -0300

    [12] Include validations if input is not str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
     num = 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i, char in enumerate(roman_string):
