ff9aa2d05fc4ef4229eebd92dbea09ca252e23fb
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-09 14:42:33 -0300

    [1] Trying to not use else

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    length = 0
     try:
         if type(value) == int:
             print("{:d}".format(value))
             return True
-    except ValueError():
+    except (ValueError(), TypeError()):
+        print("Not an integer")
         return False
