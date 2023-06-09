e98d24963bd25fe0c00dfa994e292fdb5108e920
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-09 14:50:40 -0300

    [1] Use ininstance() instead of type()

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if type(value) == int:
+        if isinstance(value, int):
             print("{:d}".format(value))
             return True
     except (ValueError(), TypeError()):
-        print("Not an integer")
-    else:
+        print("Error: The value is not an integer")
         return False
