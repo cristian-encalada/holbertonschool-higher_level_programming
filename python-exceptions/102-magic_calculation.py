0e2e5d4cf65c647f1ea087dfdfcd543e448b9b70
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-10 21:20:44 -0300

    [9] Bytecode -> Python #4

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                Exception('too far')
+            result = a ** b
+            result += i
+        except Exception:
+            result = a + b
+        finally:
+            return result
