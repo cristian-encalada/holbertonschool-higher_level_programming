4268523c731a570cffc26b2761ff729ec2caa544
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-12 17:59:01 -0300

    [0] Add module documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+''' Task 0. Write an empty class Square
+
+    Requiremnts:
+    -----------
+    - Is not allowed to import any module
+'''
+
+
 class Square:
-    """Empty class that defines a square"""
+    '''Empty class that defines a square'''
     pass
