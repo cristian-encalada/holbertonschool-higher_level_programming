f9643ea600ce73f581efaf5ccd5480cbd69c688a
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-15 15:37:53 -0300

    [0] More edge cases: ValueError - NaN

--- python-test_driven_development/error_type.py
+++ python-test_driven_development/error_type.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 try:
+    result = int(float('inf'))
+except OverflowError as e:
+    print("Error type:", type(e).__name__)
+    print("Error message:", str(e))
+try:
     result = int(float('-inf'))
+except OverflowError as e:
+    print("Error type:", type(e).__name__)
+    print("Error message:", str(e))
+try:
+    result = int(float('NaN'))
 except OverflowError as e:
     print("Error type:", type(e).__name__)
     print("Error message:", str(e))
