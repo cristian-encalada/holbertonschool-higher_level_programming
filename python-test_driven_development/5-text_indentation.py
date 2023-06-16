07f93cc4ce551533e46a3a563a9deb37022ca666
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-16 15:36:56 -0300

    [4] Add more tests. Edge cases

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Task 5. Text indentation
+"""
+
+
 def text_indentation(text):
+    """
+    Print a text with 2 new lines after each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
