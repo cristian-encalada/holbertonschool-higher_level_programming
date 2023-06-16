c0864cf9e6782420dcf0c77f2f2be75416b7c340
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-16 17:23:13 -0300

    [5] Add more tests. Edge cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -22,8 +22,25 @@ class TestMaxInteger(unittest.TestCase):
     def test_mixed(self):
         self.assertEqual(max_integer([-5, -8, -1, 14]), 14)
 
-    def test_mixed(self):
+    def test_chars(self):
         self.assertEqual(max_integer(['w', 'x', 'y', 'z']), 'z')
+
+    def test_empty(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_inf(self):
+        self.assertEqual(max_integer([5, float("inf")]), float("inf"))
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([8]), 8)
+
+    def test_string(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, "test"])
+
+    def test_more_args(self):
+        with self.assertRaises(TypeError):
+            max_integer([5, 8, "test"], 5)
 
 
 if __name__ == "__main__":
