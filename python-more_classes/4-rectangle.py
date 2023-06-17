ca1d4d1da80d532d16686f52611941cd66a72ad1
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-17 13:56:03 -0300

    Minor improvements

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -83,4 +83,4 @@ class Rectangle:
 
     def __repr__(self):
         "Return a string representation of the rectangle"
-        return f'Rectangle({self.__width}, {self.__height})'
+        return f"Rectangle({self.__width}, {self.__height})"
