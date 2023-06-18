edfe594e57157cb6d6a00b6eb0706d1e5a9f8c64
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-18 18:12:33 -0300

    [7] Fix: pycodestyle, line more than 79 chars

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -46,7 +46,10 @@ class SinglyLinkedList:
             self.head = new_node
         else:
             current = self.head
-            while current.next_node is not None and value >= current.next_node.data:
+            while (
+                current.next_node is not None and
+                value >= current.next_node.data
+            ):
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
