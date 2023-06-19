8e3fe6eb250a0eaa213b8e4d4cf7717fad6d9831
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-18 22:21:43 -0300

    [7] Add alias to import

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
+"""Task 7. Lazy multiplication
 """
-Task 7. Lazy multiplication
-"""
-
-
-import numpy
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """ Multiply 2 matrices using the module NumPy
     """
-    return numpy.dot(m_a, m_b)
+    return np.dot(m_a, m_b)
