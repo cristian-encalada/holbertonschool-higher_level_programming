858ce1354b807ff8186416fe7806da157f29b6a2
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-20 15:09:22 -0300

    [3] Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Task 3. Same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Return True if the object is an instance of,
+        or if the object is an instance of a class that inherited from
+    """
+    return True if isinstance(obj, a_class) else False
