a2967cb0588cc0147a5a960d7a0e74fe743e101b
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-20 16:57:18 -0300

    [6] Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Task 6. Improve Geometry
+"""
+
+
+class BaseGeometry:
+    """ Class BaseGeometry based on 5-base_geometry.py
+    """
+    pass
+
+    def area(self):
+        """ Not implemented yet
+        """
+        raise Exception('area() is not implemented')
