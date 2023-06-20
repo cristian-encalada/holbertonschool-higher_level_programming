8eae00ebd7d0b1c6a251dc291ba69be0a1853a82
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-20 17:36:14 -0300

    [7] Use type() instead of isinstance()

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """ Validate value, assume <name> is always a string
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(name + ' must be an integer')
         if value <= 0:
             raise ValueError(name + ' must be greater than 0')
