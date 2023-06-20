a8450e3df69a4af2b8ce324b16b52c499fbb89b5
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-20 18:22:32 -0300

    [7] Use format() to print the error

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" Task 7. Integer validator
+"""Task 7. Integer validator
 """
 
 
 class BaseGeometry:
-    """ Class BaseGeometry based on 6-base_geometry.py
+    """Class BaseGeometry based on 6-base_geometry.py
     """
 
     def area(self):
-        """ Not implemented yet
+        """Not implemented yet
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Validate value, assume <name> is always a string
+        """Validate value, assume name is always a string
         """
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
