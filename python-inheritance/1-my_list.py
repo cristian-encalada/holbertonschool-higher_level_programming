ac635b03a44c3bf2bd88d192f413f83a00b76e1c
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-20 14:28:16 -0300

    [1] My list. Add class documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Task 1. My list
+""" Task 1. My list
 """
 
 
 class MyList(list):
+    """ Print a list sorted in ascending order
+    """
     def print_sorted(self):
         print(sorted(self))
