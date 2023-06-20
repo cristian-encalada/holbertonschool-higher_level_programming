bd6a5f4ef96a0bac8a38b8c9c8be32149ec9de41
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-20 15:19:55 -0300

    [4] Only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Task 4. Only sub class of
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Return True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
