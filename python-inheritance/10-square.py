da82ee25d4df2de4149c0500bfafd5aaef6dc6c9
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-20 21:13:46 -0300

    [10] Square 1. Change super() init position

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,6 +10,6 @@ class Square(Rectangle):
     def __init__(self, size):
         """ Instantiation
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
