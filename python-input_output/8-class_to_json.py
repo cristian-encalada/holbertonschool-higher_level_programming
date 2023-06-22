69a33d12ad76bb5dcd9a50b90416e87cbd390a44
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-22 20:33:19 -0300

    [8] Not use import json and json.dumps

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Task 8. Class to JSON"""
-import json
 
 
 def class_to_json(obj):
     """Return the dictionary description with simple data structure
     for JSON serialization of an object
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
