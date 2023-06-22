b8f06b212d8c2a14e482bf740423e7b20e7813b1
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-22 15:09:21 -0300

    [0] Add encoding uft-8 to open()

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,8 +4,8 @@
 
 
 def read_file(filename=""):
-    """Read a text file (UTF8) and prints it to stdout
+    """Read a text file (UTF8) and print it to stdout
     """
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         content = file.read()
-        print(content)
+        print(content, end='')
