f7e3d77067664ad31cccf11a9a08a58f534f4e09
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-22 14:06:38 -0300

    [0] Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Task 0. Read file
+"""
+
+
+def read_file(filename=""):
+    """Read a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, 'r') as file:
+        content = file.read()
+        print(content)
