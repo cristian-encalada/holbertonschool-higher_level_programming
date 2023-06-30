f9cd487ff2e592906be8d6a81f0fb58c35d07942
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-30 15:36:47 -0300

    [9] Update #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -118,16 +118,18 @@ class Rectangle(Base):
         rs += (' ' * self.__x + '#' * self.__width + '\n') * self.__height
         print(rs, end='')
 
-    def update(self, *args):
-        """Assigns an argument to each attribute of the Rectangle instance"""
-        if len(args) > 0:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.width = args[1]
-            if len(args) >= 3:
-                self.height = args[2]
-            if len(args) >= 4:
-                self.x = args[3]
-            if len(args) >= 5:
-                self.y = args[4]
+    def update(self, *args, **kwargs):
+        """Assigns key-value arguments attribute of the Rectangle instance"""
+        for key, value in kwargs.items():
+            if key == 'id':
+                self.id = value
+            elif key == 'width':
+                self.width = value
+            elif key == 'height':
+                self.height = value
+            elif key == 'x':
+                self.x = value
+            elif key == 'y':
+                self.y = value
+            else:  # If *args exists and is not empty, skip **kwargs
+                pass
