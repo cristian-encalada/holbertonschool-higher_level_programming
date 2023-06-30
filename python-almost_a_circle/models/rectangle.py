052b495a3bc2883acc3ee21303c83ee62dea5a0f
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-06-30 15:24:26 -0300

    [8] Update #0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -113,7 +113,21 @@ class Rectangle(Base):
         )
 
     def display(self):
-        """Prints the Rectangle instance to stdout"""
+        """Prints in stdout the Rectangle instance with # char """
         rs = '\n' * self.__y
         rs += (' ' * self.__x + '#' * self.__width + '\n') * self.__height
         print(rs, end='')
+
+    def update(self, *args):
+        """Assigns an argument to each attribute of the Rectangle instance"""
+        if len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
