c99e8908496c1e2e8e7498ed261193e2f29d41a7
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-01 23:35:42 -0300

    Add simple tests for base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,9 +6,18 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-class Test_init(unittest.TestCase):
-    "Unit tests for models/base.py"
+class TestBase_Instantiation(unittest.TestCase):
+    """Unit tests for models/base.py"""
 
-    def test_id_right(self):
-        base = Base(123)
-        self.assertEqual(base.id, 123)
+    def test_id_no_duplicates(self):
+        """Check no duplicated values for id"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        b5 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 4)
