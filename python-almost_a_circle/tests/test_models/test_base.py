21369f3aed090267ed0862c888fc1756c226c76a
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-02 18:30:05 -0300

    Simple test for base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"Unit tests for models/base.py"
+"""Unit tests for models/base.py class"""
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
-class TestBase_Instantiation(unittest.TestCase):
-    """Unit tests for models/base.py"""
+class TestBase(unittest.TestCase):
+    """Test class for Base class"""
 
-    def test_id_no_duplicates(self):
-        """Check no duplicated values for id"""
+    def test_new_instances_with_id(self):
+        """New instances with id"""
         b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        b4 = Base(12)
-        b5 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base()
         self.assertEqual(b2.id, 2)
+        b3 = Base()
         self.assertEqual(b3.id, 3)
+        b4 = Base(12)
         self.assertEqual(b4.id, 12)
+        b5 = Base()
         self.assertEqual(b5.id, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
