6146ce9b77e2242e44ac6782afbbf66bb9f739cf
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-24 10:49:04 -0300

    [3] List tables

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- Script that lists all the tables of a database in MySQL server
+-- The database name will be passed as argument of mysql command
+-- For example: mysql -hlocalhost -uroot -p <database_name>
+SHOW tables;
