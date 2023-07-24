a8a43ab7f4c45ec2d2945f55db61dfc47f1692c8
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-24 10:55:51 -0300

    [4] First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Script that creates a table called first_table in the current database
+-- first_table description: 
+--      id INT
+--      name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+    id int,
+    name varchar(256)
+);
