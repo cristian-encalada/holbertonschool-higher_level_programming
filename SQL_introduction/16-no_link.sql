468d0d612ac8d887877d54ffbcf657cf011f0604
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-24 15:47:08 -0300

    [16] Say my name

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,14 @@
+-- Script  that lists the number of records with the same score 
+-- in the table second_table of the database hbtn_0c_0
+-- The result should display:
+--     the score
+--    the number of records for this score with the label number
+-- The list should be sorted by the number of records (descending)
+-- The database name will be passed as an argument to the mysql command
+SELECT 
+    score, name
+FROM
+    second_table
+WHERE 
+    name IS NOT NULL
+ORDER BY score DESC;
