69a307aa1fe260b71fc3406490e232f29cd33ca8
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-25 15:40:27 -0300

    [16] List shows and genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,10 @@
--- Script  that lists all Comedy shows in the database hbtn_0d_tvshows.
---    The tv_genres table contains only one record where name = Comedy (but the id can be different)
---    Each record should display: tv_shows.title
---    Results must be sorted in ascending order by the show title
+-- Script  that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+--    If a show doesn’t have a genre, display NULL in the genre column
+--    Each record should display: tv_shows.title - tv_genres.name
+--    Results must be sorted in ascending order by the show title and genre name
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
