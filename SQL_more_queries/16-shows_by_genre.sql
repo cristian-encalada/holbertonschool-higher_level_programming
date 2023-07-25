fc5218ae7cfac89aaa1af880956a59def6e3a9c0
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-25 15:32:53 -0300

    [15] Improve JOIN statements

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,8 +5,8 @@
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
