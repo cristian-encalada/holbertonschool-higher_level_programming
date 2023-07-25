60ac999ac289e5f5d5ad06634f4749481defb907
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-25 12:05:52 -0300

    [5] Unique ID

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Script that creates the table unique_id
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, the script should not fail
+CREATE TABLE IF NOT EXISTS unique_id (
+    id int DEFAULT 1 NOT NULL UNIQUE,
+    name varchar(256)
+);
