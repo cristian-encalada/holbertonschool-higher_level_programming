84b551db24bd35ae50ba38723682cdfbb4a6e551
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-27 12:18:40 -0300

    [3] Fix. Update cursor.execute

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """Script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
+states table of hbtn_0e_0_usa where name matches the argument
 It must be safe from MySQL injections"""
 
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    # Connect to the database
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    # Execute the SQL query
-    query = ("""SELECT * FROM states WHERE name
-            LIKE BINARY '%{}%' ORDER BY id""".format(argv[4]))
-    cur.execute(query)
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cur.execute(query, (argv[4],))
     states = cur.fetchall()
-    # Print each state
     for state in states:
         print(state)
     db.close()
