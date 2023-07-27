dbfe93913585f09da28ba535035b6baed7ee8e98
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-27 11:27:49 -0300

    [1] Fix. Use BINARY to change LIKE proper comparison

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Script that lists all states with a name starting with N"""
-import MySQLdb
-from sys import argv
-
 
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
     # Connect to the database
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
-    # Create a cursor to interact with the database
     cur = db.cursor()
     # Execute the SQL query
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id"
     cur.execute(query)
-    # Fetch all rows as a list
     states = cur.fetchall()
     # Print each state
     for state in states:
         print(state)
-    # Close the database connection
     db.close()
