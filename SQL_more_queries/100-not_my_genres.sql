bfb2eb8605a65c830a82c376f995fbbb4bd53527
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-29 23:48:47 -0300

    [17] Not my genre

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -3,18 +3,10 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 -- Use a maximum of two SELECT statement
-SELECT DISTINCT name
-FROM tv_genres AS g
-INNER JOIN tv_show_genres AS s
-ON g.id = s.genre_id
-INNER JOIN tv_shows AS t
-ON s.show_id = t.id
-WHERE g.name NOT IN
-(SELECT name
-FROM tv_genres AS g
-INNER JOIN tv_show_genres AS s
-ON g.id = s.genre_id
-INNER JOIN tv_shows AS t
-ON s.show_id = t.id
-WHERE t.title = "Dexter")
-ORDER BY g.name;
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT genre_id FROM tv_show_genres
+    WHERE show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+    )
+ORDER BY name ASC;
