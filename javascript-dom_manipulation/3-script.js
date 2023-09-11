d06d58531c90134defc2a034271999ef8ad0f195
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-11 19:23:29 -0300

    [3] Use let instead of const for header element

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,12 +1,12 @@
 // Toggles the class of the header element when the user clicks on the tag id toggle_header
-const header = document.querySelector('header');
+let header = document.querySelector('header');
 const toggleHeader = document.getElementById('toggle_header');
 toggleHeader.addEventListener('click', function onClick (event) {
-  if (header.classList.contains('red')) {
-    header.classList.remove('red');
-    header.classList.add('green');
-  } else {
+  if (header.classList.contains('green')) {
     header.classList.remove('green');
     header.classList.add('red');
+  } else {
+    header.classList.remove('red');
+    header.classList.add('green');
   }
 });
