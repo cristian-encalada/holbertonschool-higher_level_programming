3ce0efbdb348564e9d4c3f56bb795f099483c49d
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-18 17:20:02 -0300

    [6] Change the text, done

--- javascript-web_jquery/5-script.js
+++ javascript-web_jquery/5-script.js
@@ -1,8 +1,8 @@
 // Add a <li> element to a list when the user clicks on the tag DIV#add_item
 // The new element must be: <li>Item</li>
 // The new element must be added to UL.my_list
-$(document).ready(function() {
-  $('#add_item').click(function() {
+$(document).ready(function () {
+  $('#add_item').click(function () {
     const newItem = $('<li>Item</li>');
     $('ul.my_list').append(newItem);
   });
